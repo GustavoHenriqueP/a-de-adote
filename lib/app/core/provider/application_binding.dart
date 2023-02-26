c563656f6b6bec26bf9fe007ea71ee9cf1eb9254
Gustavo Henrique Araújo de Paula <gustavo_henriquedepaula@hotmail.com>
2023-02-26 20:39:22 -0300

    Add AuthRepository provider

--- lib/app/core/provider/application_binding.dart
+++ lib/app/core/provider/application_binding.dart
@@ -1,4 +1,5 @@
 import 'package:a_de_adote/app/core/rest_client/custom_dio.dart';
+import 'package:a_de_adote/app/repositories/auth/auth_repository.dart';
 import 'package:a_de_adote/app/repositories/auth/auth_repository_impl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -17,6 +18,9 @@ class ApplicationBinding extends StatelessWidget {
       providers: [
         Provider(
           create: (context) => CustomDio(),
+        ),
+        Provider<AuthRepository>(
+          create: (context) => AuthRepositoryImpl(),
         ),
       ],
       child: child,
