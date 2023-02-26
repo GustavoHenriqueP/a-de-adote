7b812633c065684a04b7f8db39270a9c8357014f
Gustavo Henrique Araújo de Paula <gustavo_henriquedepaula@hotmail.com>
2023-02-26 20:05:44 -0300

    Keeping implementing auth

--- lib/app/repositories/ong/ong_repository.dart
+++ lib/app/repositories/ong/ong_repository.dart
@@ -3,6 +3,6 @@ import '../../models/ong_model.dart';
 abstract class OngRepository {
   Future<OngModel> getOngDataFromWeb(String cnpj);
   Stream<OngModel> getOng(String ongId);
-  Future<void> createOng(OngModel ong, String uid);
+  Future<void> createOng(OngModel ong);
   Future<void> updateOng(OngModel ong);
 }
